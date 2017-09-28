b5e8c34b5000babe4a074bac54b187b9f04538e9
sanjaynainanixlls <sanjay@xlls.co>
2017-09-28 14:42:37 +0530

    Facade Design Pattern

---
+++ Facade/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Facade
+{
+    public interface ITemperatureService
+    {
+        LocalTemperature GetTemperature(double farenheit);
+    }
+
+    public class LocalTemperature
+    {
+        public double Celcius { get; set; }
+        public double Farenheit { get; set; }
+    }
+
+    public class TemperatureService : ITemperatureService
+    {
+        readonly MetricConverterService _converter;
+
+        public TemperatureService() : this(new MetricConverterService())
+        { }
+
+        public TemperatureService(MetricConverterService converter)
+        {
+            _converter = converter;
+        }
+
+        public LocalTemperature GetTemperature(double farenheit)
+        {
+            double celcius = _converter.FarenheitToCelcius(farenheit);
+            LocalTemperature localTemperature = new LocalTemperature()
+            {
+                Farenheit = farenheit,
+                Celcius = celcius,
+            };
+
+            return localTemperature;
+        }
+    }
+
+    public class MetricConverterService
+    {
+        public double FarenheitToCelcius(double degrees)
+        {
+            return ((degrees - 32) / 9.0) * 5.0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double temperature = 77.5;
+
+            ITemperatureService temperatureService = new TemperatureService();
+            LocalTemperature localTemp = temperatureService.GetTemperature(temperature);
+
+            Console.WriteLine("The current temperature is {0}F/{1}C.",
+                                localTemp.Farenheit.ToString("F1"),
+                                localTemp.Celcius.ToString("F1"));
+
+            Console.ReadKey();
+        }
+    }
+}
